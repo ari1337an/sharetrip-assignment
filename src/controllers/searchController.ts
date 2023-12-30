830bdcc01e136c416e798f24ba7d54fc5d986308
Md Sahadul Hasan Arian <ari1337an@gmail.com>
2023-12-30 23:45:31 +0600

    fix: ip address extraction from express request

--- src/controllers/searchController.ts
+++ src/controllers/searchController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import helper from "@/utils/helper";
 import config from "@/utils/config";
 import PostSearchUseCase from "@/infrastructure/usecases/PostSearchUseCase";
+import PrismaSearchActionRepoStrategy from "@/infrastructure/repositories/PrismaSearchActionRepoStrategy";
 
 /**
  * ExpressJS Request handler function that fetches keyword key
@@ -20,10 +21,18 @@ export default async function searchController(
     // Retrieve keyword from Request
     const keyword = request.query?.keyword as string;
 
+    // Retrieve user IP address
+    const userIp = request?.ip as string
+
+    // Repository (For Dependency injection)
+    const repository = new PrismaSearchActionRepoStrategy()
+
     // Send data to UseCases which will generate response
     const filteredPosts = await new PostSearchUseCase().execute(
       keyword,
-      config.EXTERNAL_API
+      userIp,
+      config.EXTERNAL_API,
+      repository
     );
 
     // Return response
