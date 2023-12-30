5bd3d18516e9a6894a3e41b286c027fb22c94c15
Md Sahadul Hasan Arian <ari1337an@gmail.com>
2023-12-30 23:41:18 +0600

    feat: adding SearchActionEntity and its zod validation

--- src/infrastructure/entities/SearchActionEntity.ts
+++ src/infrastructure/entities/SearchActionEntity.ts
@@ -1,22 +1,28 @@
-import { z, ZodError } from "zod";
+import { z, ZodError, ZodDate } from "zod";
 import PostEntity from "@/infrastructure/entities/PostEntity";
 
 export default class SearchActionEntity {
-  uid: string | null;
-  matches: PostEntity[];
+  uid: string;
+  timestamp: string; 
+  userIP: string;
+  searchresults: PostEntity[];
 
   constructor(data: {
-    uid: number;
-    matches: PostEntity[];
+    uid: string;
+    timestamp: string;
+    userIP: string;
+    searchresults: PostEntity[];
   }) {
 
     try {
-      // Validate entity using zod 
-      const validatedData = SearchActionEntity.schema().parse(data);
+      // Validate entity using Zod
+      const validatedData = (SearchActionEntity.schema()).parse(data);
 
       // Set the field values
-      this.uid = validatedData.uid;
-      this.matches = validatedData.matches;
+      this.uid = validatedData.uid as string;
+      this.timestamp = validatedData.timestamp as string;
+      this.userIP = validatedData.userIP as string;
+      this.searchresults = validatedData.searchresults as PostEntity[];
     } catch (error) {
       if (error instanceof ZodError) {
         console.error("SearchActionEntity Validation error:", error.errors);
@@ -27,10 +33,12 @@ export default class SearchActionEntity {
     }
   }
 
-  static schema () {
+  static schema() {
     return z.object({
-      uid: z.string().nullable(),
-      matches: z.array(z.instanceof(PostEntity))
+      uid: z.string().min(0),
+      timestamp: z.string(),
+      userIP: z.string().ip(),
+      searchresults: z.array(z.instanceof(PostEntity)),
     });
   }
 }
