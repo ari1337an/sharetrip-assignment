50c30ef7a4f18168f8c452d703e22b4d007ea550
Md Sahadul Hasan Arian <ari1337an@gmail.com>
2023-12-31 01:17:48 +0600

    fix: fixing production module alias of typescript

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,10 @@
-import 'module-alias/register'; 
-
 // import components
 import http from "http";
 import app from "./app";
 import config from "./utils/config";
 import logger from "./utils/logger";
 
-/**
- * Module-Alias Configuration:
- * - Creating the '@' alias for the './src' folder to simplify import paths
- *   and improve code readability.
- * 
- * Usage:
- * - In other files, leverage '@' for importing modules from the 'src' folder.
- *   Example: '@/utils/config' resolves to 'ROOT/src/utils/config'.
- */
-
-
-// prepare 
+// prepare
 const server = http.createServer(app);
 const port = app.get("port");
 const callback = () => {
